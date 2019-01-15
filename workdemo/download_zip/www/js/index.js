b7e4883624d819e6a2a488bb36de9abfe8bed76d
greatabel <myreceiver2for2github@gmail.com>
2019-01-15 17:33:15 +0800

    download path

--- workdemo/download_zip/www/js/index.js
+++ workdemo/download_zip/www/js/index.js
@@ -3,11 +3,21 @@ document.addEventListener("deviceready", init, false);
 function init() {
 
     //listen for button clicks
+    // https://www.npmjs.com/package/cordova-plugin-file-downloader
     document.querySelector("#downloadTest").addEventListener("touchend", 
         function() {
+            var tank0_url = "https://lumistatic.blob.core.chinacloudapi.cn/rinnegandemo/tank0.zip";
+            downloader.init({folder: "testApp", unzip: true, delete: false});
+            downloader.get(tank0_url);
 
-            navigator.notification.alert(
-                "This is a test...", null, "Alert Test", "OK!");
+            document.addEventListener("DOWNLOADER_downloadSuccess", function(event){
+                  var data = event.data;
+                  navigator.notification.alert(
+                // cordova.file.cacheDirectory + "/testApp/" + data,  null, "Alert Test", "OK!");
+                cordova.file.cacheDirectory + "/testApp/" + "tank0" + JSON.stringify(data),  null, "Alert Test", "OK!");
+            });
+            // navigator.notification.alert(
+            //     "下载中...", null, "Alert Test", "OK!");
 
     }, false);
 
