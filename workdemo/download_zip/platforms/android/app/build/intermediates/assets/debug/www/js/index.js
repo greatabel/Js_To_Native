4d660539033d1a1e62f8893f4f4cc5470d7aa57f
greatabel <myreceiver2for2github@gmail.com>
2019-01-17 17:23:25 +0800

    fix 2nd not working bug

--- workdemo/download_zip/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ workdemo/download_zip/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,21 +1,32 @@
 document.addEventListener("deviceready", init, false);
 
 function init() {
-
+    downloader.init({folder: "testApp", fileSystem: cordova.file.cacheDirectory, unzip: true, delete: false});
     //listen for button clicks
     // https://www.npmjs.com/package/cordova-plugin-file-downloader
     document.querySelector("#downloadTest").addEventListener("touchend", 
         function() {
             var tank0_url = "https://lumistatic.blob.core.chinacloudapi.cn/rinnegandemo/tank0.zip";
-            downloader.init({folder: ".", unzip: true, delete: false});
+            // folder 为 . 是当前目录，不再创建一个子文件夹
+            // downloader.init({folder: "testApp", fileSystem: cordova.file.cacheDirectory, unzip: true, delete: false});
             downloader.get(tank0_url);
-
-            document.addEventListener("DOWNLOADER_downloadSuccess", function(event){
+            var  handler = function (event){
+                 document.removeEventListener("DOWNLOADER_downloadSuccess", handler);
                   var data = event.data;
                   navigator.notification.alert(
                 // cordova.file.cacheDirectory + "/testApp/" + data,  null, "Alert Test", "OK!");
                 // cordova.file.cacheDirectory + "/testApp/" + "tank0" + JSON.stringify(data),  null, "Alert Test", "OK!");
-                cordova.file.cacheDirectory + "/testApp/" + "tank0 data=" + JSON.stringify(data),  null, "Alert Test", "OK!");  
+                cordova.file.cacheDirectory + "/testApp/" + "tank0 data=" + JSON.stringify(data),  null, "Alert Test", "OK!");
+  
+            };
+            document.addEventListener("DOWNLOADER_downloadSuccess", handler);
+
+            document.addEventListener("DOWNLOADER_error", function(event){
+                  var data = event.data;
+                  navigator.notification.alert(
+                // cordova.file.cacheDirectory + "/testApp/" + data,  null, "Alert Test", "OK!");
+                // cordova.file.cacheDirectory + "/testApp/" + "tank0" + JSON.stringify(data),  null, "Alert Test", "OK!");
+                cordova.file.cacheDirectory + "error =" , data,  null, "Alert Test", "OK!");  
             });
             // navigator.notification.alert(
             //     "下载中...", null, "Alert Test", "OK!");
